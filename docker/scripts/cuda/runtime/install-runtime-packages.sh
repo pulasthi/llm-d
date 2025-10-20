3c6cdb57a67774f7b52d9c5d92db2b6a52cacb4b
Will Eaton <weaton@redhat.com>
2025-10-20 09:42:32 -0400

    support cuda ubuntu
    Signed-off-by: Will Eaton <weaton@redhat.com>

--- docker/scripts/cuda/runtime/install-runtime-packages.sh
+++ docker/scripts/cuda/runtime/install-runtime-packages.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -Eeu
 
-# installs base packages, EPEL/universe repos, and CUDA repository
+# installs runtime packages for CUDA image
 #
 # Required environment variables:
 # - PYTHON_VERSION: Python version to install (e.g., 3.12)
 # - CUDA_MAJOR: CUDA major version (e.g., 12)
+# - CUDA_MINOR: CUDA minor version (e.g., 9)
 # - TARGETOS: Target OS - either 'ubuntu' or 'rhel' (default: rhel)
 
 TARGETOS="${TARGETOS:-rhel}"
@@ -21,18 +22,20 @@ fi
 # shellcheck source=docker/scripts/cuda/common/package-utils.sh
 source "$UTILS_SCRIPT"
 
-MAPPINGS_FILE=$(find_mappings_file "builder-package-mappings.json" "$SCRIPT_DIR")
+MAPPINGS_FILE=$(find_mappings_file "runtime-package-mappings.json" "$SCRIPT_DIR")
 DOWNLOAD_ARCH=$(get_download_arch)
 
 # main installation logic
 if [ "$TARGETOS" = "ubuntu" ]; then
     setup_ubuntu_repos
+    update_system ubuntu
     mapfile -t INSTALL_PKGS < <(load_and_expand_packages ubuntu "$MAPPINGS_FILE")
     install_packages ubuntu "${INSTALL_PKGS[@]}"
     cleanup_packages ubuntu
 
 elif [ "$TARGETOS" = "rhel" ]; then
     setup_rhel_repos "$DOWNLOAD_ARCH"
+    update_system rhel
     mapfile -t INSTALL_PKGS < <(load_and_expand_packages rhel "$MAPPINGS_FILE")
     install_packages rhel "${INSTALL_PKGS[@]}"
     cleanup_packages rhel
